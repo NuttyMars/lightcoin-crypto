a66b7a9c37f01111017f472b8cee8e58d04f5216
Natalia Martian <natalia.martian@gmail.com>
2020-11-08 04:28:43 +0000

    Updated initial logic to extend functionality of app

--- index.js
+++ index.js
@@ -1,16 +1,82 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+  //all accounts will start with this
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
   }
 
-  commit() {
-    balance -= this.amount;
+  get balance() {
+    let balance = 0;
+
+    for (const transaction of this.transactions) {
+      balance += transaction.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
   }
 
 }
+
+class Transaction {
+
+  //this is common to both types of transactions
+  //amount = amount transacted
+  //account = object created with the Account class (stored in a variable)
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+
+    if (this.isAllowed()) {
+      // //will add or subtract the amount transacted - obsolete
+    // this.account.balance += this.value;
+    //keep track of the time of transaction
+      this.time = new Date();
+      //this replaces the earlier adding of this.value to the balance
+      //because addTransaction adds it to the array
+      //and get balance updates the balance
+      this.account.addTransaction(this);
+    } else {
+      return false;
+    }
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  //the amount transacted
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    if (this.amount > 0) {
+      return true;
+    }
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  //the amount transacted
+  get value() {
+    return -this.amount;
+  }
+
+  isAllowed() {
+    if (this.amount <= this.account.balance) {
+      return true;
+    }
+  }
+}
+
 
 
 
@@ -18,12 +84,27 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+console.log('Account:', myAccount);
+console.log('Starting Balance:', myAccount.balance);
+console.log('----');
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
+console.log('Updated Balance:', myAccount.balance);
+console.log('----');
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
+console.log('Updated Balance:', myAccount.balance);
+console.log('----');
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(9.99, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+console.log('Updated Balance:', myAccount.balance);
+console.log('----');
+
+console.log('Ending Balance:', myAccount.balance);
